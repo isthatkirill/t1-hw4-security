8f528c9978ca3895f3b85a3fbae82285fab46d6c
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-06-05 00:31:52 +0300

    init: project initialized via spring initializr

---
+++ src/main/java/isthatkirill/hwfoursecurity/HwFourSecurityApplication.java
@@ -0,0 +1,13 @@
+package isthatkirill.hwfoursecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwFourSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwFourSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isthatkirill/hwfoursecurity/HwFourSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package isthatkirill.hwfoursecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwFourSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
