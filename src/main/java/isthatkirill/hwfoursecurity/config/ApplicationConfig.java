de690d0c4a10edefc59548ec9480c47d5468b6f3
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-06-05 18:32:25 +0300

    docs: OpenApi bean configured

---
+++ src/main/java/isthatkirill/hwfoursecurity/config/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package isthatkirill.hwfoursecurity.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                )
+                ).info(new Info()
+                        .title("HW №4 API")
+                        .description("API documentation for HW №4 application")
+                        .version("1.0"));
+    }
+
+}
